d428905e0dfae3e0db2d6f0a1be08b5de9f5505c
jeka1978 <kit2009>
2022-01-13 12:52:37 +0200

    Initial commit

---
+++ src/test/java/com/idi/ididiscovery/IdiDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idi.ididiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdiDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
